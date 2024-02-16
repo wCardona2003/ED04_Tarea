00ff2abdaf43284f0dd5311c4db20c77b1289351
wCardona2003 <wildermancardona@paucasesnovescifp.cat>
2024-02-16 21:56:20 +0100

    Agregamos parametro cantidad tipo float al metodo operativa cuenta

--- src/main/java/paucasesnoves/main/Main.java
+++ src/main/java/paucasesnoves/main/Main.java
@@ -6,12 +6,12 @@ public class Main {
 
     public static void main(String[] args) {
         CCuenta cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
-        operativa_cuenta(cuenta1);
+        operativa_cuenta(cuenta1, 695f); // Utiliza 695f en lugar de 695 para representar un float
     }
 
-    public static void operativa_cuenta(CCuenta cuenta1) {
+    public static void operativa_cuenta(CCuenta cuenta1, float cantidad) { // Corregido a float
         double saldoActual = cuenta1.estado();
-        System.out.println("El saldo actual es " + saldoActual);
+        System.out.println("El saldo actual es " + saldoActual); // Agrega un espacio después de "es"
 
         try {
             cuenta1.retirar(2300);
@@ -20,9 +20,10 @@ public class Main {
         }
         try {
             System.out.println("Ingreso en cuenta");
-            cuenta1.ingresar(695);
+            cuenta1.ingresar(cantidad);
         } catch (Exception e) {
             System.out.print("Fallo al ingresar");
         }
     }
+
 }
