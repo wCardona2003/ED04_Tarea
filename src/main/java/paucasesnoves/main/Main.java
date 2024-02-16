a70bf679794195d9e61eb50ca04b89b8d6f76497
wCardona2003 <wildermancardona@paucasesnovescifp.cat>
2024-02-16 21:54:31 +0100

    Encapsulamos los atributos de CCuenta

--- src/main/java/paucasesnoves/main/Main.java
+++ src/main/java/paucasesnoves/main/Main.java
@@ -5,13 +5,13 @@ import paucasesnoves.main.ccuenta.CCuenta;
 public class Main {
 
     public static void main(String[] args) {
-        CCuenta cuenta1 = new CCuenta("Antonio López","1000-2365-85-1230456789",2500,0);
+        CCuenta cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
         operativa_cuenta(cuenta1);
     }
 
-    public static void operativa_cuenta(CCuenta cuenta1){
+    public static void operativa_cuenta(CCuenta cuenta1) {
         double saldoActual = cuenta1.estado();
-        System.out.println("El saldo actual es "+ saldoActual );
+        System.out.println("El saldo actual es " + saldoActual);
 
         try {
             cuenta1.retirar(2300);
@@ -25,6 +25,4 @@ public class Main {
             System.out.print("Fallo al ingresar");
         }
     }
-
 }
-
